1223f55027ed548abb2edaa2dcb12e67292f9dc7
xwang345 <ryan.wang23@hotmail.com>
2019-09-30 01:03:51 -0400

    Finish the project basic

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A Test Recipe',
+    'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg'),
+    new Recipe('A Test Recipe', 'A Test Recipe',
+    'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name: string, amount: number) {
+  //  this.name = name;
+  //  this.amount = amount;
+  // } The TypeScript is intruduce a new way to write the simllar thing
+  constructor(public name: string, public amount: number) {}
+}
